96752fa5c6e9fee25f30306cf3367abcedf9d1f2
Megan Wang <mxwang@ucdavis.edu>
2020-06-07 19:57:05 -0700

    Additional comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/* VERSION 1
+// to remember states, you use 'state'
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      // by calling this.setState  from onClick, tell React to rerender everytime button is clicked
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+*/
+
+/* VERSION 2
+no longer needs a constructor because square doesn't need to keep track of the game's state
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+*/
+
+// change Square from component to function component -> doesn't have their own state
+// takes props as input and returns what should be rendered
 function Square(props) {
     return (
       <button className="square" onClick={props.onClick}>
@@ -11,9 +53,54 @@ function Square(props) {
   }
   
   class Board extends React.Component {
+    /*
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares : Array(9).fill(null);
+        XisNext = true;
+      };
+    }
+
+    //.slice -> to create a copy of the squares array to modify instead of modifying the actual array itself
+    handleClick(i) {
+      //checks if there is already a winner; if there is, then do nothing
+      if (calculateWinner(squares) || squares[i]) {
+      return;
+      }
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+      
+    }
+
+
+    // each square receives a value prop that is X, O, or null
     renderSquare(i) {
+      return <Square value={this.state.squares[i]} />;
+    }
+     */
+
+     //passing down 2 props from Board to Square
+     /*
+      renderSquare(i) {
       return (
         <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
+    }
+     */
+
+
+    //passing props is how info flows; from Board parent -> square child
+    renderSquare(i) {
+      return (
+        <Square 
           value={this.props.squares[i]}
           onClick={() => this.props.onClick(i)}
         />
@@ -122,11 +209,9 @@ function Square(props) {
       );
     }
   }
-  
-  // ========================================
-  
   ReactDOM.render(<Game />, document.getElementById("root"));
   
+  //given an array of 9 items, function checks for a winner and returns O,X, or null
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
